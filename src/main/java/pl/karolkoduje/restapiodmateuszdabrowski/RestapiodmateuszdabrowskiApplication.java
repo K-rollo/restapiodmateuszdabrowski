891741208085c3cd050b99b592c8bbb0b4e51f65
Karol <karolkania@wp.pl>
2023-04-22 15:26:11 +0200

    Initial commit

---
+++ src/main/java/pl/karolkoduje/restapiodmateuszdabrowski/RestapiodmateuszdabrowskiApplication.java
@@ -0,0 +1,13 @@
+package pl.karolkoduje.restapiodmateuszdabrowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiodmateuszdabrowskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiodmateuszdabrowskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/karolkoduje/restapiodmateuszdabrowski/RestapiodmateuszdabrowskiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karolkoduje.restapiodmateuszdabrowski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiodmateuszdabrowskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
